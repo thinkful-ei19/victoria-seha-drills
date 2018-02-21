9c723ed171a82b925b20b1a85030e2663b1b9131
Victoria Daug <victoria.daug@gmail.com>
2018-02-21 19:16:11 +0000

    added summer school

--- drill1.js
+++ drill1.js
@@ -1,58 +1,91 @@
-function createMyObject(){
-	return{
-	foo : 'bar',
-	answerToUniverse : 42,
-	'olly olly' : 'oxen free',
-	sayHello : function(){
-	return 'hello';
-	}
+// function createMyObject(){
+// 	return{
+// 	foo : 'bar',
+// 	answerToUniverse : 42,
+// 	'olly olly' : 'oxen free',
+// 	sayHello : function(){
+// 	return 'hello';
+// 	}
+//
+// 	}
+// }
+// console.log(createMyObject);
+//
+// function updateObject(obj) {
+// 	obj.foo = 'foo';
+// 	obj.bar = 'bar';
+// 	obj.bizz = 'bizz';
+// 	obj.bang = 'bang';
+// 	return obj;
+// }
+// console.log(updateObject());
+//
+// function personMaker() {
+//   var person = {
+//     firstName: 'Paul',
+//     lastName: 'Jones',
+//     fullName: function() {
+//     	return this.firstName + ' ' + this.lastName;
+//     },
+//   };
+//   return person;
+// }
+// console.log(personMaker());
+//
+// function keyDeleter(obj) {
+//   delete obj.foo;
+// 	delete obj.bar;
+// 	return obj;
+// }
+//
+// var sampleObj = {
+//   foo: 'foo',
+//   bar: 'bar',
+//   bizz: 'bizz',
+//   bang: 'bang',
+// };
+// console.log(keyDeleter(sampleObj))
+//
+// function makeStudentReport(data){
+// 	const outcome = [];
+// 	for(let i = 0; i < data.length; i++){
+// 		const item = data[i];
+// 		outcome.push(`${item.name}: ${item.grade}`);
+// 	}
+//     return outcome;
+// }
+// var data = {name: 'Johnny Robot', grade: 'C'};
+// console.log(makeStudentReport(data));
+//
+//
 
-	}
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students) {
+ const result = [];
+ for(i = 0; i < students.length; i++){
+	 result.push({
+		 name: students[i].name,
+		 status: 'In Summer Schooll',
+		 course: students[i].course,
+		 });
+	 }
+	 return result;
 }
-console.log(createMyObject);
-
-function updateObject(obj) {
-	obj.foo = 'foo';
-	obj.bar = 'bar';
-	obj.bizz = 'bizz';
-	obj.bang = 'bang';
-	return obj;
-}
-console.log(updateObject());
-
-function personMaker() {
-  var person = {
-    firstName: 'Paul',
-    lastName: 'Jones',
-    fullName: function() {
-    	return this.firstName + ' ' + this.lastName;
-    },
-  };
-  return person;
-}
-console.log(personMaker());
-
-function keyDeleter(obj) {
-  delete obj.foo;
-	delete obj.bar;
-	return obj;
-}
-
-var sampleObj = {
-  foo: 'foo',
-  bar: 'bar',
-  bizz: 'bizz',
-  bang: 'bang',
-};
-console.log(keyDeleter(sampleObj))
-
-function makeStudentReport(data){
-	const outcome = [];
-	for(let i = 0; i < data.length; i++){
-		const item = data[i];
-		outcome.push(`${item.name}: ${item.grade}`);
-	}
-    return outcome;
-}
-var data = {name: 'Johnny Robot', grade: 'C'};
-console.log(makeStudentReport(data));
+console.log(enrollInSummerSchool(studentData))
