b8b60fa3d29aab1a32154939bb039712519db295
Seha Dalay <sehagulnar@gmail.com>
2018-02-21 16:18:07 -0500

    Update drill2.js
    uncomment

--- drill2.js
+++ drill2.js
@@ -1,32 +1,32 @@
-// const loaf = {
-// 	flour: 300,
-// 	water: 210,
-// }
-// console.log(loaf.flour);
-// console.log(loaf.water);
+const loaf = {
+	flour: 300,
+	water: 210,
+}
+console.log(loaf.flour);
+console.log(loaf.water);
 
 
-// const loaf = {
-// 	flour: 300,
-// 	water: 210,
+const loaf = {
+	flour: 300,
+	water: 210,
 
-//   hydration : function() {
-// 	return (loaf.water / loaf.flour) * 100;
-// }
-// }
-// console.log(loaf.hydration());
+  hydration : function() {
+	return (loaf.water / loaf.flour) * 100;
+}
+}
+console.log(loaf.hydration());
 
-// const obj = {
-// 	foo: 1,
-// 	bar: 10,
-// 	fum: 100,
-// 	quux: 200,
-// 	spam: 500
-// }
+const obj = {
+	foo: 1,
+	bar: 10,
+	fum: 100,
+	quux: 200,
+	spam: 500
+}
 
-// for (const prop in obj) {
-// 	console.log(`obj.${prop} = ${obj[prop]}`)
-// }
+for (const prop in obj) {
+	console.log(`obj.${prop} = ${obj[prop]}`)
+}
 
 
 const hobbit = { meals : ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']}
