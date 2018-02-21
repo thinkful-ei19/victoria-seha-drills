eba05e48a166fa2edfffc487ba05f1f0d1000cdc
Victoria Daug <victoria.daug@gmail.com>
2018-02-21 17:12:53 +0000

    deleting obj

--- drill1.js
+++ drill1.js
@@ -31,3 +31,17 @@ function personMaker() {
   return person;
 }
 console.log(personMaker());
+
+function keyDeleter(obj) {
+  delete obj.foo;
+	delete obj.bar;
+	return obj;
+}
+
+var sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+console.log(keyDeleter(sampleObj))
