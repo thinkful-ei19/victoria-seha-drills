2a425e4040a3d01320ae0d340c8aa9e0a5eeb818
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-02-21 14:31:59 -0500

    Added find by id func

--- drill1.js
+++ drill1.js
@@ -1,63 +1,63 @@
-// function createMyObject(){
-// 	return{
-// 	foo : 'bar',
-// 	answerToUniverse : 42,
-// 	'olly olly' : 'oxen free',
-// 	sayHello : function(){
-// 	return 'hello';
-// 	}
-//
-// 	}
-// }
-// console.log(createMyObject);
-//
-// function updateObject(obj) {
-// 	obj.foo = 'foo';
-// 	obj.bar = 'bar';
-// 	obj.bizz = 'bizz';
-// 	obj.bang = 'bang';
-// 	return obj;
-// }
-// console.log(updateObject());
-//
-// function personMaker() {
-//   var person = {
-//     firstName: 'Paul',
-//     lastName: 'Jones',
-//     fullName: function() {
-//     	return this.firstName + ' ' + this.lastName;
-//     },
-//   };
-//   return person;
-// }
-// console.log(personMaker());
-//
-// function keyDeleter(obj) {
-//   delete obj.foo;
-// 	delete obj.bar;
-// 	return obj;
-// }
-//
-// var sampleObj = {
-//   foo: 'foo',
-//   bar: 'bar',
-//   bizz: 'bizz',
-//   bang: 'bang',
-// };
-// console.log(keyDeleter(sampleObj))
-//
-// function makeStudentReport(data){
-// 	const outcome = [];
-// 	for(let i = 0; i < data.length; i++){
-// 		const item = data[i];
-// 		outcome.push(`${item.name}: ${item.grade}`);
-// 	}
-//     return outcome;
-// }
-// var data = {name: 'Johnny Robot', grade: 'C'};
-// console.log(makeStudentReport(data));
-//
-//
+function createMyObject(){
+	return{
+	foo : 'bar',
+	answerToUniverse : 42,
+	'olly olly' : 'oxen free',
+	sayHello : function(){
+	return 'hello';
+	}
+
+	}
+}
+console.log(createMyObject);
+
+function updateObject(obj) {
+	obj.foo = 'foo';
+	obj.bar = 'bar';
+	obj.bizz = 'bizz';
+	obj.bang = 'bang';
+	return obj;
+}
+console.log(updateObject());
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function() {
+    	return this.firstName + ' ' + this.lastName;
+    },
+  };
+  return person;
+}
+console.log(personMaker());
+
+function keyDeleter(obj) {
+  delete obj.foo;
+	delete obj.bar;
+	return obj;
+}
+
+var sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+console.log(keyDeleter(sampleObj))
+
+function makeStudentReport(data){
+	const outcome = [];
+	for(let i = 0; i < data.length; i++){
+		const item = data[i];
+		outcome.push(`${item.name}: ${item.grade}`);
+	}
+    return outcome;
+}
+var data = {name: 'Johnny Robot', grade: 'C'};
+console.log(makeStudentReport(data));
+
+
 
 const studentData = [
   {
@@ -89,3 +89,14 @@ function enrollInSummerSchool(students) {
 	 return result;
 }
 console.log(enrollInSummerSchool(studentData))
+
+  function findById(items, idNum){
+  for(let i = 0; i < items.length; i++){
+    if(items[i].id === idNum){
+      return items[i];
+    }
+  }
+}
+const data = [{id: 1, foo: 'bar'}, {id: 2, foo: 'bizz'}];
+findById(data, 2);
+
