87f2b72b9d609a5662063a502354deba441338c5
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-02-21 16:58:32 -0500

    properties arent there

--- drill2.js
+++ drill2.js
@@ -1,38 +1,38 @@
-// const loaf = {
-// 	flour: 300,
-// 	water: 210,
-// }
-// console.log(loaf.flour);
-// console.log(loaf.water);
-//
-//
-// const loaf = {
-// 	flour: 300,
-// 	water: 210,
-//
-//   hydration : function() {
-// 	return (loaf.water / loaf.flour) * 100;
-// }
-// }
-// console.log(loaf.hydration());
-//
-// const obj = {
-// 	foo: 1,
-// 	bar: 10,
-// 	fum: 100,
-// 	quux: 200,
-// 	spam: 500
-// }
-//
-// for (const prop in obj) {
-// 	console.log(`obj.${prop} = ${obj[prop]}`)
-// }
-//
-//
-// const hobbit = { meals : ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']}
-//
-// console.log(hobbit.meals[3]);
-//
+const loaf = {
+	flour: 300,
+	water: 210,
+}
+console.log(loaf.flour);
+console.log(loaf.water);
+
+
+const loaf = {
+	flour: 300,
+	water: 210,
+
+  hydration : function() {
+	return (loaf.water / loaf.flour) * 100;
+}
+}
+console.log(loaf.hydration());
+
+const obj = {
+	foo: 1,
+	bar: 10,
+	fum: 100,
+	quux: 200,
+	spam: 500
+}
+
+for (const prop in obj) {
+	console.log(`obj.${prop} = ${obj[prop]}`)
+}
+
+
+const hobbit = { meals : ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']}
+
+console.log(hobbit.meals[3]);
+
 
 const arr = [
 	{name: 'Seha', job: 'Developer'},
@@ -42,4 +42,20 @@ const arr = [
 
 for (let i = 0; i < arr.length; i++){
 	console.log(arr[i])
+}
+
+
+const arr = [
+	{name: 'Seha', job: 'Developer', boss: 'Fred'},
+	{name: 'Victoria', job: 'Web Developer', boss: 'Mike'},
+	{name: 'Rich', job: 'Teacher'}
+];
+
+for (let i = 0; i < arr.length; i++){
+	if(arr[i].boss !== undefined){
+	  console.log(`${arr[i].name} reports to ${arr[i].boss}.`);
+}
+else{
+    console.log(`${arr[i].name} doesn't report to anybody.`);
+}
 }
