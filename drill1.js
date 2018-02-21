90d8394dd3c18f3deff25061d309e575974cdd62
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-02-21 12:07:54 -0500

    Added personMakerfunction

--- drill1.js
+++ drill1.js
@@ -19,3 +19,15 @@ function updateObject(obj) {
 	return obj;
 }
 console.log(updateObject());
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    fullName: function() {
+    	return this.firstName + ' ' + this.lastName;
+    },
+  };
+  return person;
+}
+console.log(personMaker());
