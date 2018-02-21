b2681a9ec499152b9e2e6bcfcd4cba4829a57136
Victoria Daug <victoria.daug@gmail.com>
2018-02-21 21:27:22 +0000

    job plus name arr obj

--- drill2.js
+++ drill2.js
@@ -1,35 +1,45 @@
-const loaf = {
-	flour: 300,
-	water: 210,
+// const loaf = {
+// 	flour: 300,
+// 	water: 210,
+// }
+// console.log(loaf.flour);
+// console.log(loaf.water);
+//
+//
+// const loaf = {
+// 	flour: 300,
+// 	water: 210,
+//
+//   hydration : function() {
+// 	return (loaf.water / loaf.flour) * 100;
+// }
+// }
+// console.log(loaf.hydration());
+//
+// const obj = {
+// 	foo: 1,
+// 	bar: 10,
+// 	fum: 100,
+// 	quux: 200,
+// 	spam: 500
+// }
+//
+// for (const prop in obj) {
+// 	console.log(`obj.${prop} = ${obj[prop]}`)
+// }
+//
+//
+// const hobbit = { meals : ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']}
+//
+// console.log(hobbit.meals[3]);
+//
+
+const arr = [
+	{name: 'Seha', job: 'Developer'},
+	{name: 'Victoria', job: 'Web Developer'},
+	{name: 'Rich', job: 'Teacher'}
+]
+
+for (let i = 0; i < arr.length; i++){
+	console.log(arr[i])
 }
-console.log(loaf.flour);
-console.log(loaf.water);
-
-
-const loaf = {
-	flour: 300,
-	water: 210,
-
-  hydration : function() {
-	return (loaf.water / loaf.flour) * 100;
-}
-}
-console.log(loaf.hydration());
-
-const obj = {
-	foo: 1,
-	bar: 10,
-	fum: 100,
-	quux: 200,
-	spam: 500
-}
-
-for (const prop in obj) {
-	console.log(`obj.${prop} = ${obj[prop]}`)
-}
-
-
-const hobbit = { meals : ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']}
-
-console.log(hobbit.meals[3]);
-
